b5f1a819bbbc006b4a8b6d39f3caebb5b959ef91
Jay Stotz <jason.stotz@gmail.com>
2019-04-09 23:23:33 -0400

    Remove unused default_context for now

--- lib/jstreams.rb
+++ lib/jstreams.rb
@@ -3,9 +3,5 @@ require 'jstreams/version'
 require 'jstreams/context'
 
 module Jstreams
-  def self.default_context
-    @default_context ||= Context.new
-  end
-
   class Error < StandardError; end
 end
