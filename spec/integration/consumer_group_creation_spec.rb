68af5337f84803268945804f34a3a38adab82a02
Jay Stotz <jason.stotz@gmail.com>
2019-04-12 01:19:30 -0400

    Integration specs for consumer group creation

---
+++ spec/integration/consumer_group_creation_spec.rb
@@ -0,0 +1,34 @@
+RSpec.describe 'consumer group creation' do
+  before(:each) { redis.flushdb }
+  after(:each) { jstreams.shutdown }
+
+  let(:jstreams) { Jstreams::Context.new }
+  let(:redis) { Redis.new }
+
+  context 'when the consumer group does not exist' do
+    it 'creates a new stream and consumer group on start' do
+      jstreams.publish('mystream', 'foo')
+      expect(redis.xinfo(:groups, 'mystream')).to eq([])
+      jstreams.subscribe('mygroup', 'mystream') { |_, _, s| s.stop }
+      Timeout.timeout(5) { jstreams.run }
+      expect(redis.xinfo(:groups, 'mystream').map { |g| g['name'] }).to eq(
+            %w[mygroup]
+          )
+    end
+  end
+
+  context 'when the consumer group already exists' do
+    it 'does nothing' do
+      redis.xgroup(:create, 'mystream', 'mygroup', 0, mkstream: true)
+      jstreams.publish('mystream', 'foo')
+      expect(redis.xinfo(:groups, 'mystream').map { |g| g['name'] }).to eq(
+            %w[mygroup]
+          )
+      jstreams.subscribe('mygroup', 'mystream') { |_, _, s| s.stop }
+      Timeout.timeout(5) { jstreams.run }
+      expect(redis.xinfo(:groups, 'mystream').map { |g| g['name'] }).to eq(
+            %w[mygroup]
+          )
+    end
+  end
+end
